94d4e0222bd77258a23a3970304848962089db43
AshVivier <ash.set.vivier@gmail.com>
2019-04-21 14:46:13 +1200

    add Mihi Tips to nav bar

--- components/TopNavBar.jsx
+++ components/TopNavBar.jsx
@@ -7,6 +7,7 @@ const TopNavBar = () => (
   <a href="https://maoridictionary.co.nz/"><strong>Maori Dictionary</strong></a>
   <a href="http://www.maorilanguage.net/how-to-pronounce-maori/"><strong>Pronunciation</strong></a>
   <a href="https://www.toastmasters.org/resources/public-speaking-tips"><strong>Public speaking tips</strong></a>
+<a href="https://www.otago.ac.nz/maori/world/te-reo-maori/mihi-introductions/index.html"><strong> Mihi Tips</strong></a>
   {/* <Route path="/Mountain" component={Mountain}/> */}
 </div>
 )
