af208859d4fff677231edaf8d2eada6a675c7a5c
AshVivier <ash.set.vivier@gmail.com>
2019-04-22 18:20:34 +1200

    Add some routes

--- components/Mountain.jsx
+++ components/Mountain.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Mountain = (props) => (
   <div className= 'Mountain PepehaPage' >
@@ -9,9 +10,10 @@ const Mountain = (props) => (
         <h3>Mountain</h3>
       </form>
 
+      <Link to='./Water' ><button className='CreateButton'><strong>Next</strong></button></Link>
+
   </div>
 )
-
 
 
 export default Mountain 
